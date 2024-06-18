125851a4730d64ddba47819afba724662d046a1b
Benjamin Romero <programador.ben@gmail.com>
2024-06-18 08:08:49 -0400

    Auto-generated commit

---
+++ chunk-AMRF4TNC.js
@@ -0,0 +1,1 @@
+var p={production:!1,apiUrl:"https://asistencia-426800.rj.r.appspot.com/",apiVersion:"v1",apiBase:"api",apiBaseUrl:"https://asistencia-426800.rj.r.appspot.com/api/"};export{p as a};
--- chunk-KKHEBOWH.js
+++
@@ -1,1 +0,0 @@
-var a={production:!1,apiUrl:"http://192.168.126.131:8080",apiVersion:"v1",apiBase:"api",apiBaseUrl:"http://localhost:8080/api/"};export{a};
