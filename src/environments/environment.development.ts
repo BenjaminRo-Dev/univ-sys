263766646ba87b8d59ba77be1979c2596563e2cd
Benjamin Romero <programador.ben@gmail.com>
2024-06-18 08:07:47 -0400

    Conexion a la nube

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,10 @@
 export const environment = {
     production: false,
     // apiUrl: 'http://localhost:8080',
-    // apiUrl: 'https://asistencia-426800.rj.r.appspot.com/',
-    apiUrl: 'http://192.168.126.131:8080',
+    apiUrl: 'https://asistencia-426800.rj.r.appspot.com/',
+    // apiUrl: 'http://192.168.126.131:8080',
     apiVersion: 'v1',
     apiBase: 'api',
-    apiBaseUrl: 'http://localhost:8080/api/',
-    // apiBaseUrl: 'https://asistencia-426800.rj.r.appspot.com/api/',
+    // apiBaseUrl: 'http://localhost:8080/api/',
+    apiBaseUrl: 'https://asistencia-426800.rj.r.appspot.com/api/',
 };
